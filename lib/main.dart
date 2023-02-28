5e8847d8c231fc3157296b689eb79bbaef093941
qashqyn <qashqyn291@gmail.com>
2023-03-01 01:48:24 +0600

    module 3

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Module 3 '),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> uriNames = [
+    'https://cdn.worldvectorlogo.com/logos/flutter.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/f/fe/Dart_programming_language_logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/9/95/Android_Studio_Icon_3.6.svg'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(height: 400.0),
+              items: uriNames.map((uriName) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 40.0),
+                        child: SvgPicture.network(
+                          uriName,
+                          semanticsLabel: 'A shark?!',
+                          placeholderBuilder: (BuildContext context) =>
+                              Container(
+                                  padding: const EdgeInsets.all(50.0),
+                                  child: const CircularProgressIndicator()),
+                        ));
+                  },
+                );
+              }).toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
