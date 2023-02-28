41b7c881c482b7526bd593ee60765742500f4c5f
qashqyn <qashqyn291@gmail.com>
2023-03-01 03:03:11 +0600

    Module 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
-// Package imports:
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -15,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Skillbox Flutter Course',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Module 3 '),
+      home: const MyHomePage(title: 'Module 4'),
     );
   }
 }
@@ -34,54 +30,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> uriNames = [
-    'https://cdn.worldvectorlogo.com/logos/flutter.svg',
-    'https://upload.wikimedia.org/wikipedia/commons/f/fe/Dart_programming_language_logo.svg',
-    'https://upload.wikimedia.org/wikipedia/commons/9/95/Android_Studio_Icon_3.6.svg',
-    'assets/svg/skillbox_logo.svg'
-  ];
+  Map<String, dynamic> data = {
+    'Мои фото': [
+      'https://picsum.photos/1200/501',
+      'https://picsum.photos/1200/502',
+      'https://picsum.photos/1200/503',
+      'https://picsum.photos/1200/504',
+      'https://picsum.photos/1200/505',
+      'https://picsum.photos/1200/506',
+      'https://picsum.photos/1200/507',
+      'https://picsum.photos/1200/508',
+      'https://picsum.photos/1200/509',
+      'https://picsum.photos/1200/510',
+      'https://picsum.photos/1200/526',
+      'https://picsum.photos/1200/527',
+      'https://picsum.photos/1200/528',
+      'https://picsum.photos/1200/529',
+      'https://picsum.photos/1200/530',
+      'https://picsum.photos/1200/531',
+    ],
+    'Галерея': [
+      'https://picsum.photos/1200/511',
+      'https://picsum.photos/1200/512',
+      'https://picsum.photos/1200/513',
+      'https://picsum.photos/1200/514',
+      'https://picsum.photos/1200/515',
+      'https://picsum.photos/1200/516',
+      'https://picsum.photos/1200/517',
+      'https://picsum.photos/1200/518',
+      'https://picsum.photos/1200/519',
+      'https://picsum.photos/1200/520',
+      'https://picsum.photos/1200/521',
+      'https://picsum.photos/1200/522',
+      'https://picsum.photos/1200/523',
+      'https://picsum.photos/1200/524',
+      'https://picsum.photos/1200/525',
+      'https://picsum.photos/1200/532',
+    ]
+  };
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CarouselSlider(
-              options: CarouselOptions(height: 400.0),
-              items: uriNames.map((uriName) {
-                SvgPicture svgPicture;
-                if (Uri.parse(uriName).isAbsolute) {
-                  svgPicture = SvgPicture.network(
-                    uriName,
-                    placeholderBuilder: (BuildContext context) => Container(
-                        padding: const EdgeInsets.all(50.0),
-                        child: const CircularProgressIndicator()),
+    return DefaultTabController(
+      length: data.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Галерея'),
+          bottom: TabBar(
+              tabs: data.entries.map((el) => Tab(text: el.key)).toList()),
+        ),
+        body: TabBarView(
+          children: data.entries.map((el) {
+            return GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 2,
+                crossAxisSpacing: 2,
+              ),
+              key: PageStorageKey(el.key),
+              children: <Widget>[
+                ...el.value.map((e) {
+                  return Image.network(
+                    e,
+                    fit: BoxFit.cover,
                   );
-                } else {
-                  svgPicture = SvgPicture.asset(
-                    uriName,
-                    placeholderBuilder: (BuildContext context) => Container(
-                        padding: const EdgeInsets.all(50.0),
-                        child: const CircularProgressIndicator()),
-                  );
-                }
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: const EdgeInsets.symmetric(horizontal: 40.0),
-                        child: svgPicture);
-                  },
-                );
-              }).toList(),
-            )
-          ],
+                }).toList()
+              ],
+            );
+          }).toList(),
         ),
       ),
     );
